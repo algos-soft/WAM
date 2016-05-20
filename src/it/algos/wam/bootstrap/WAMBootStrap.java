37fad58f1080a619fdb114db3238c7fe7a2e1709
algos-soft <info@algos.it>
2016-05-20 15:31:18 +0200

    .

--- src/it/algos/wam/bootstrap/WAMBootStrap.java
+++ src/it/algos/wam/bootstrap/WAMBootStrap.java
@@ -40,8 +40,8 @@ public class WAMBootStrap extends ABootStrap {
         WAMApp.setServletContext(svltCtx);
 
         // eventuali modifiche allle dimensioni dei bottoni
-        Toolbar.ALTEZZA_BOTTONI = 40;
-        Toolbar.LARGHEZZA_BOTTONI = 140;
+//        Toolbar.ALTEZZA_BOTTONI = 40;
+//        Toolbar.LARGHEZZA_BOTTONI = 140;
         //TextField.LARGHEZZA_DEFAULT="320px";  // non trova questa variabile - alex 01-03-16
 
         // eventuali modifiche ai flag generali di regolazione
