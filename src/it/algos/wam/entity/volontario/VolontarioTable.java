bb89e86a91334a9c5609f82e831a0c3fa901c0ee
GuidoCeresa <gac@algos.it>
2016-05-25 15:51:44 +0200

    .

--- src/it/algos/wam/entity/volontario/VolontarioTable.java
+++ src/it/algos/wam/entity/volontario/VolontarioTable.java
@@ -1,13 +1,15 @@
 package it.algos.wam.entity.volontario;
 
-import com.vaadin.addon.jpacontainer.EntityContainer;
 import com.vaadin.data.Container;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 import it.algos.wam.entity.companyentity.WamCompanyEntity_;
 import it.algos.wam.entity.funzione.Funzione;
 import it.algos.webbase.multiazienda.ETable;
 import it.algos.webbase.web.lib.LibSession;
+import it.algos.webbase.web.lib.LibText;
 import it.algos.webbase.web.module.ModulePop;
-import org.vaadin.addons.lazyquerycontainer.LazyEntityContainer;
 
 import java.util.ArrayList;
 
@@ -17,41 +19,75 @@ import java.util.ArrayList;
  */
 public class VolontarioTable extends ETable {
 
-    private ArrayList<String> colonne = new ArrayList();
 
     public VolontarioTable(ModulePop module) {
         super(module);
+        inizia();
+    }// end of constructor
+
+
+    private void inizia() {
         Container cont = getContainerDataSource();
         if (cont instanceof Container.Sortable) {
             Container.Sortable sortable = (Container.Sortable) cont;
             sortable.sort(new Object[]{Volontario_.cognome.getName()}, new boolean[]{true});
         }// end of if cycle
-    }// end of constructor
+
+    }// end of method
+
+
+    @Override
+    protected void createAdditionalColumns() {
+        ArrayList<Funzione> listaFunzioni = Funzione.findAll();
+        for (Funzione funz : listaFunzioni) {
+            addGeneratedColumn(funz.getSiglaInterna(), new FunzioniColumnGenerator(funz));
+        }// end of for cycle
+    }// end of method
 
 
     protected Object[] getDisplayColumns() {
-        ArrayList<Funzione> listaFunzioni= Funzione.findAll();
+        ArrayList lista = new ArrayList<>();
+        ArrayList<Funzione> listaFunzioni = Funzione.findAll();
 
         if (LibSession.isDeveloper()) {
-            return new Object[]{
-                    WamCompanyEntity_.company,
-                    Volontario_.nome,
-                    Volontario_.cognome
-            };
-        } else {
-            return new Object[]{
-                    Volontario_.nome,
-                    Volontario_.cognome
-            };
-        }// end of if/else cycle
+            lista.add(WamCompanyEntity_.company);
+        }// end of if cycle
+
+        lista.add(Volontario_.nome);
+        lista.add(Volontario_.cognome);
+
+        for (Funzione funz : listaFunzioni) {
+            lista.add(funz.getSiglaInterna());
+        }// end of for cycle
+
+        return lista.toArray();
     }// end of method
 
+
     /**
-     * Refreshes the underlying container from the database
+     * Colonna generata.
      */
-    @Override
-    public void refresh() {
-        int a=87;
-    }// end of method
+    private class FunzioniColumnGenerator implements ColumnGenerator {
+
+        private Funzione funz;
+
+        /**
+         * Costruttore minimo con tutte le properties obbligatorie
+         */
+        public FunzioniColumnGenerator(Funzione funz) {
+            this.funz = funz;
+        }// end of constructor
+
+        /**
+         * Genera la cella.
+         */
+        public Component generateCell(Table source, Object itemId, Object columnId) {
+            String tag = funz.getSiglaInterna();
+            tag = tag.substring(0, 1);
+            tag = LibText.primaMaiuscola(tag);
+
+            return new Label(tag);
+        }// end of method
+    }// end of inner class
 
 }// end of class
