8a3bbe78760a5c4ad23ae972248a70284639de26
GuidoCeresa <gac@algos.it>
2016-06-30 15:51:27 +0200

    .

--- src/it/algos/wam/entity/volontario/VolontarioTable.java
+++ src/it/algos/wam/entity/volontario/VolontarioTable.java
@@ -68,11 +68,12 @@ public class VolontarioTable extends ETable {
 
         lista.add(Volontario_.nome);
         lista.add(Volontario_.cognome);
+        lista.add(Volontario_.admin);
 
-        if (LibSession.isDeveloper()||LibSession.isAdmin()) {
-            lista.add(Volontario_.admin);
-        }// end of if cycle
-
+//        if (LibSession.isDeveloper()||LibSession.isAdmin()) {
+//            lista.add(Volontario_.admin);
+//        }// end of if cycle
+//
         for (Funzione funz : listaFunzioni) {
             lista.add(funz.getSiglaInterna());
         }// end of for cycle
